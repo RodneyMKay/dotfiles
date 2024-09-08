09def72582ba950f9443fe442d130a9278432f60
RodneyMKay <36546810+RodneyMKay@users.noreply.github.com>
2024-09-08 18:55:27 +0200

    Disable podman warning

--- hosts/janpc/home.nix
+++ hosts/janpc/home.nix
@@ -1,6 +1,15 @@
 { config, pkgs, stateVersion, inputs, ... }: {
   home.stateVersion = stateVersion;
 
+  home.sessionVariables = {
+    # FIXME: This should also be in a module.
+    #  See more details in configuration.nix.
+    #  The reason this is needed is that
+    #  cgroupsv2 is installed on the system,
+    #  but not detected by podman.
+    PODMAN_IGNORE_CGROUPSV1_WARNING = "1";
+  };
+
   imports = [
     ../../modules
   ];
